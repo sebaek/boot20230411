21f1cc4b8116818f04cb47c6594c607aa59268a4
SeungEun Baek <sebaek@gmail.com>
2023-04-11 15:51:24 +0900

    type level requestmapping

--- src/main/java/com/example/demo/controller/Controller01.java
+++ src/main/java/com/example/demo/controller/Controller01.java
@@ -15,6 +15,11 @@ public class Controller01 {
 	public void method02() {
 		System.out.println("/sub1/link2 경로 요청에서 일하는 메소드!!!!");
 	}
+	
+	@RequestMapping("/sub1/link3")
+	public void method03() {
+		System.out.println("/sub1/link3 요청에서 일하는 메소드!!");
+	}
 }
 
 
