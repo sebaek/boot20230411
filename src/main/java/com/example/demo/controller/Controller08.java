0a5be0dca09a80cb32896437ced3926c2a50c6cd
SeungEun Baek <sebaek@gmail.com>
2023-04-12 16:37:22 +0900

    @modelattribute

--- src/main/java/com/example/demo/controller/Controller08.java
+++ src/main/java/com/example/demo/controller/Controller08.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.domain.*;
@@ -23,9 +24,54 @@ public class Controller08 {
 		obj.setCompany("apple");
 		obj.setModel("ipad");
 		obj.setPrice(300);
+	}
+	
+	@RequestMapping("link3")
+	public void method3(Model model) {
+		Dto02 dto02 = new Dto02();
+		Dto03 dto03 = new Dto03();
+		model.addAttribute("product", dto02);
+		model.addAttribute("member", dto03);
+	}
+	
+	@RequestMapping("link4")
+	public void method4(@ModelAttribute("product") Dto02 dto02,
+						@ModelAttribute("member") Dto03 dto03) {
+		//3. add attribute
+		//4. forward
 		
 	}
+	
+	// model attribute의 이름을 생략하면 클래스이름을 lowerCamelCase
+	
+	// 예) CompanyMember -> compnayMember
+	@RequestMapping("link5")
+	public void method5(@ModelAttribute("dto02") Dto02 dto02,
+						@ModelAttribute("dto03") Dto03 dto03) {
+		//3. add attribute
+		dto02.setCompany("microsoft");
+		dto03.setName("bill gates");
+		//4. forward
+		
+	}
+	
+	@RequestMapping("link6")
+	public String method6(@ModelAttribute Dto02 dto02,
+						@ModelAttribute Dto03 dto03) {
+		//3. add attribute
+		dto02.setCompany("apple");
+		dto03.setName("steve jobs");
+		//4. forward
+		return "/sub8/link5";
+	}
 }
+
+
+
+
+
+
+
 
 
 
