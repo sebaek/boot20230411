417b513f6dc4b45be5b6ca27dfd49eebea6ac9dd
SeungEun Baek <sebaek@gmail.com>
2023-04-12 10:54:09 +0900

    view

--- src/main/java/com/example/demo/controller/Controller06.java
+++ src/main/java/com/example/demo/controller/Controller06.java
@@ -18,12 +18,12 @@ public class Controller06 {
 		System.out.println("link1 메소드 일함");
 		return "forward:/sub6/link2";
 	}
-	
+
 	@RequestMapping("link2")
 	public void method2() {
 		System.out.println("link2 메소드 일함");
 	}
-	
+
 	// 경로 : /sub6/link3
 	// method3 작성
 	// forward /sub6/link2
@@ -36,52 +36,59 @@ public class Controller06 {
 		// 4 forward
 		return "forward:/sub6/link2";
 	}
-	
+
 	@RequestMapping("link4")
-	public void method4(HttpServletRequest req, HttpServletResponse res) 
+	public void method4(HttpServletRequest req, HttpServletResponse res)
 			throws Exception {
-		
+
 		System.out.println("link4 에서 일함");
-		
+
 		String view = "/WEB-INF/views/abc.jsp";
 		req.getRequestDispatcher(view).forward(req, res);
 	}
-	
+
 	@RequestMapping("link5")
 	public String method5() {
-		
+
 		System.out.println("link5 에서 일함");
-		
+
 		return "forward:/WEB-INF/views/abc.jsp";
 	}
-	
+
 	@RequestMapping("link6")
 	public String method6() {
 		System.out.println("link6  에서 일함");
 		return "abc";
 	}
-	
+
 	// 경로 : /sub6/link7
 	// method7 작성
 	// forward /WEB-INF/views/def.jsp
 	@RequestMapping("link7")
 	public String method7() {
 		System.out.println("link 7 일함.");
-		
+
 		return "def";
-		
+
 		/*
-		request.getRequestDispatcher("/WEB-INF/views/def.jsp")
-			.forward(request, response);
-		*/
+		 * request.getRequestDispatcher("/WEB-INF/views/def.jsp") .forward(request,
+		 * response);
+		 */
 	}
-	
+
+	// 경로 : /sub6/link8
+	@RequestMapping("link8")
+	public String method8() {
+		System.out.println("8번 메소드 일함");
+		// default view name : /sub6/link8
+		return "/sub6/link8";
+	}
+
+	// 경로 : /sub6/link9
+	@RequestMapping("link9")
+	public void method9() {
+		System.out.println("9번 메소드 일함");
+		// default view name : /sub6/link9
+	}
+
 }
-
-
-
-
-
-
-
-
