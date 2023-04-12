7873de7ea5fe9b6087713abe7d8cde1152d98228
SeungEun Baek <sebaek@gmail.com>
2023-04-12 10:28:00 +0900

    jsp forward

--- src/main/java/com/example/demo/controller/Controller06.java
+++ src/main/java/com/example/demo/controller/Controller06.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
+import java.io.*;
+
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 
 @Controller
 @RequestMapping("sub6")
@@ -21,6 +26,25 @@ public class Controller06 {
 	// 경로 : /sub6/link3
 	// method3 작성
 	// forward /sub6/link2
+	@RequestMapping("link3")
+	public String method3() {
+		System.out.println("link3 메소드 일함");
+		// 1
+		// 2
+		// 3
+		// 4 forward
+		return "forward:/sub6/link2";
+	}
+	
+	@RequestMapping("link4")
+	public void method4(HttpServletRequest req, HttpServletResponse res) 
+			throws Exception {
+		
+		System.out.println("link4 에서 일함");
+		
+		String view = "/WEB-INF/views/abc.jsp";
+		req.getRequestDispatcher(view).forward(req, res);
+	}
 }
 
 
