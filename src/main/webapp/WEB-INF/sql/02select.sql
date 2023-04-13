768cf19c7377f2842f04911ca15ac03262b87cf3
SeungEun Baek <sebaek@gmail.com>
2023-04-13 11:50:10 +0900

    select

--- src/main/webapp/WEB-INF/sql/02select.sql
+++ src/main/webapp/WEB-INF/sql/02select.sql
@@ -7,3 +7,6 @@ SELECT Country FROM Customers;
 SELECT CustomerName, ContactName FROM Customers;
 SELECT CustomerName, ContactName, Country FROM Customers;
 SELECT Country, CustomerName FROM Customers;
+
+-- 문제 1) Employees 테이블에서 
+-- BirthDate, FirstName, LastName 을 조회
