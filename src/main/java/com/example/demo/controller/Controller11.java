bed26cab48add89c57ea3f49d4e43eb0db30ebd2
SeungEun Baek <sebaek@gmail.com>
2023-04-13 15:17:09 +0900

    jdbc

--- src/main/java/com/example/demo/controller/Controller11.java
+++ src/main/java/com/example/demo/controller/Controller11.java
@@ -2,31 +2,50 @@ package com.example.demo.controller;
 
 import java.sql.*;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("sub11")
 public class Controller11 {
+	
+	@Value("${spring.datasource.url}")
+	private String url;
+	@Value("${spring.datasource.name}")
+	private String name;
+	@Value("${spring.datasource.password}")
+	private String password;
 
 	@RequestMapping("link1")
-	public void method1() {
+	public void method1(Model model) {
 		// 2. business logic
 		// SELECT CustomerName FROM Customers WHERE CustomerID = 1;
 		
 		String sql = "SELECT CustomerName FROM Customers WHERE CustomerID = 1";
-		
-		// db 연결
-		Connection connection = DriverManager.getConnection(url, name, password);
-		// Statement 객체 생성
-		Statement statement = connection.createStatement();
-		// 쿼리실행
-		// 쿼리실행 결과 얻고
-		ResultSet resultSet = statement.executeQuery(sql);
-		// 쿼리실행 결과 가공
-		if (resultSet.next()) {
-			String name = resultSet.getString("CustomerName");
+		try (
+			// db 연결
+			Connection connection = DriverManager.getConnection(url, name, password);
+			// Statement 객체 생성
+			Statement statement = connection.createStatement();
+			// 쿼리실행
+			// 쿼리실행 결과 얻고
+			ResultSet resultSet = statement.executeQuery(sql);
+		) {
+			// 쿼리실행 결과 가공
+			String name = "";
+			if (resultSet.next()) {
+				name = resultSet.getString("CustomerName");
+			}
+			
+			// 3. add attribute
+			model.addAttribute("customerName", name);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		
+		// 4. forward 
 		
 	}
 }
