0495b6a1f0b7572b438827530ce347b624d32c1a
SeungEun Baek <sebaek@gmail.com>
2023-04-13 12:39:57 +0900

    연산자

---
+++ src/main/webapp/WEB-INF/sql/04where.sql
@@ -0,0 +1,11 @@
+-- = 같다
+-- <> 다르다
+SELECT * FROM Customers; -- 91개
+SELECT * FROM Customers
+WHERE Country = 'Mexico'; -- 5개
+SELECT * FROM Customers
+WHERE Country <> 'Mexico'; -- 86개
+SELECT * FROM Customers
+WHERE Country != 'Mexico'; -- 86개
+
+-- 문제1) 나라가 USA가 아닌 공급자들 조회
