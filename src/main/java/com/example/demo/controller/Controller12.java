753ea6a2ab1184926b5480b2dc6fd7b9942adeca
SeungEun Baek <sebaek@gmail.com>
2023-04-14 09:33:51 +0900

    resultset

---
+++ src/main/java/com/example/demo/controller/Controller12.java
@@ -0,0 +1,82 @@
+package com.example.demo.controller;
+
+import java.sql.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("sub12")
+public class Controller12 {
+	
+	@Value("${spring.datasource.url}")
+	private String url;
+	@Value("${spring.datasource.username}")
+	private String name;
+	@Value("${spring.datasource.password}")
+	private String password;
+	
+	@RequestMapping("link1")
+	public void method1() {
+		String sql = """
+				SELECT LastName FROM Employees
+				""";
+		try {
+			Connection con = DriverManager.getConnection(url, name, password);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			try (con; stmt; rs;) {
+				System.out.println(rs.next()); // true
+				String name1 = rs.getString("lastName");
+				System.out.println(name1);
+				
+				System.out.println(rs.next()); // true
+				String name2 = rs.getString("lastName");
+				System.out.println(name2);
+				
+				System.out.println(rs.next()); // true
+				String name3 = rs.getString("lastName");
+				System.out.println(name3);
+				
+				System.out.println(rs.next()); // true
+				String name4 = rs.getString("lastName");
+				System.out.println(name4);
+				
+				System.out.println(rs.next()); // true
+				String name5 = rs.getString("lastName");
+				System.out.println(name5);
+				
+				System.out.println(rs.next()); // true
+				String name6 = rs.getString("lastName");
+				System.out.println(name6);
+				
+				System.out.println(rs.next()); // true
+				String name7 = rs.getString("lastName");
+				System.out.println(name7);
+				
+				System.out.println(rs.next()); // true
+				String name8 = rs.getString("lastName");
+				System.out.println(name8);
+				
+				System.out.println(rs.next()); // true
+				String name9 = rs.getString("lastName");
+				System.out.println(name9);
+				
+				System.out.println(rs.next()); // false
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
