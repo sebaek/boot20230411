b7efd7bbf02d5192ff43f39b3b045de8d0f0f230
SeungEun Baek <sebaek@gmail.com>
2023-04-14 15:23:28 +0900

    not

--- src/main/webapp/WEB-INF/sql/07not.sql
+++ src/main/webapp/WEB-INF/sql/07not.sql
@@ -12,3 +12,8 @@ SELECT * FROM Employees
 WHERE NOT (BirthDate >= '1950-01-01' AND BirthDate <= '1959-12-31');
 SELECT * FROM Employees 
 WHERE BirthDate < '1950-01-01' OR BirthDate > '1959-12-31';
+
+-- 1,2번 카테고리가 아닌 상품들
+SELECT * FROM Products WHERE CategoryID = 1 OR CategoryID = 2;
+SELECT * FROM Products WHERE NOT (CategoryID = 1 OR CategoryID = 2);
+SELECT * FROM Products WHERE CategoryID <> 1 AND CategoryID <> 2;
