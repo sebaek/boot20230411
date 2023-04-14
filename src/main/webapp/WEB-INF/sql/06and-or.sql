c6f71cbd64ec7b2b8bcbe479a740e90bbb380836
SeungEun Baek <sebaek@gmail.com>
2023-04-14 14:57:08 +0900

    and

--- src/main/webapp/WEB-INF/sql/06and-or.sql
+++ src/main/webapp/WEB-INF/sql/06and-or.sql
@@ -10,6 +10,12 @@ FROM Employees
 WHERE LastName >= 'M' AND BirthDate >= '1960-01-01';
 
 -- 카테고리ID가 2이고 가격이 20.00 이상인 상품들 조회
-SELECT * FROM Products;
+SELECT * FROM Products
+WHERE CategoryID = 2 AND Price >= 20.00;
+
+-- 1950-01-01 ~ 1959-12-31 사이에 태어난 직원들 조회
+SELECT * FROM Employees
+WHERE BirthDate >= '1950-01-01' AND BirthDate <= '1959-12-31';
+
 
 
