35888a4f4b32af1c4d3e208b1e95339dbb5b1210
SeungEun Baek <sebaek@gmail.com>
2023-04-14 15:39:07 +0900

    IN

--- src/main/webapp/WEB-INF/sql/09in.sql
+++ src/main/webapp/WEB-INF/sql/09in.sql
@@ -5,3 +5,21 @@ SELECT * FROM Customers
 WHERE Country IN ('Germany', 'France');
 
 -- 3, 5 번 카테고리에 속한 상품들 조회
+SELECT * FROM Products WHERE CategoryID IN (3, 5);
+SELECT * FROM Products WHERE CategoryID = 3 OR CategoryID = 5;
+
+-- 1, 3, 5, 7 번 카테고리에 속한 상품들 조회
+SELECT * FROM Products 
+WHERE CategoryID = 1
+	OR CategoryID = 3 
+	OR CategoryID = 5
+    OR CategoryID = 7;
+SELECT * FROM Products 
+WHERE CategoryID IN (1, 3, 5, 7);
+
+-- Berlin, London, Madrid, Paris 에 있는 고객들 조회
+
+
+
+
+
