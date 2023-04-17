d66f80314bf09fbe52b5fd92980482f04c7bdc26
SeungEun Baek <sebaek@gmail.com>
2023-04-17 16:36:24 +0900

    update

--- src/main/webapp/WEB-INF/sql/26update.sql
+++ src/main/webapp/WEB-INF/sql/26update.sql
@@ -15,5 +15,32 @@ SET 					      -- 변경할 컬럼과 값 (,)
 WHERE CustomerID = 1;
     
 
+-- 문제1) 1번 공급자의 이름, 도시, 나라 변경
+
+UPDATE Suppliers
+SET SupplierName = '송태섭',
+    City = 'Busan',
+    Country = 'Korea'
+WHERE SupplierID = 1;
+
+-- 
+SELECT * FROM Suppliers;
+
+SELECT * FROM Suppliers
+WHERE Country = 'USA';
+
+UPDATE Suppliers
+SET SupplierName = '채치수',
+    Country = 'France'
+WHERE Country = 'USA';
+
+SELECT * FROM Suppliers WHERE Country = 'France';
+
+SELECT * FROM Suppliers;
+UPDATE Suppliers
+SET ContactName = '북산',
+    City = 'Seoul';
+
+
 
 
