326caea1c533f72e3ca1b8724e6cfd04311c5c4a
SeungEun Baek <sebaek@gmail.com>
2023-04-17 14:20:04 +0900

    insert into

---
+++ src/main/webapp/WEB-INF/sql/21insert.sql
@@ -0,0 +1,17 @@
+-- INSERT : 테이블에 새 레코드 입력
+SELECT * FROM Customers;
+INSERT INTO Customers
+(CustomerID, CustomerName, ContactName,
+Address, City, PostalCode, Country)
+VALUES
+(92, '박지성', '두개의심장',
+'London', 'Seoul', '1234567', 'Korea');
+
+SELECT * FROM Customers ORDER BY CustomerID DESC;
+
+-- 모든 컬럼에 값을 넣을 때 컬럼명 생략 가능
+INSERT INTO Customers
+VALUES (93, '차범근', '차붐', '프랑크푸르트', 'Seoul'
+,'1234134', 'Korea');
+
+
