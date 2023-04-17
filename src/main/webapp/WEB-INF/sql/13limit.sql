2046d75104774699f414b8d442bde8caf7798149
SeungEun Baek <sebaek@gmail.com>
2023-04-17 09:54:21 +0900

    limit

--- src/main/webapp/WEB-INF/sql/13limit.sql
+++ src/main/webapp/WEB-INF/sql/13limit.sql
@@ -10,3 +10,8 @@ SELECT * FROM Customers ORDER BY CustomerID LIMIT 0, 3;
 SELECT * FROM Customers ORDER BY CustomerID LIMIT 3, 3;
 
 SELECT * FROM Customers ORDER BY CustomerID LIMIT 6, 3;
+
+-- 두번째로 나이가 많은 직원 조회
+
+-- 2번 카테고리 상품 중 두번째로 가격이 비싼 상품 조회
+
