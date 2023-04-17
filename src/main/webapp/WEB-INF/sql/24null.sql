7f6539363e48a2fbfbe8d8019d42a84535d4ad61
SeungEun Baek <sebaek@gmail.com>
2023-04-17 15:33:46 +0900

    null 연산

--- src/main/webapp/WEB-INF/sql/24null.sql
+++ src/main/webapp/WEB-INF/sql/24null.sql
@@ -6,3 +6,10 @@ WHERE CategoryID = 2
 SELECT * FROM Products 
 WHERE CategoryID = 2 
    AND Price < 25.00; -- 8
+SELECT * FROM Products
+WHERE CategoryID = 2
+   AND IFNULL(Price, 0) < 25.00; -- 10   
+   
+   
+   
+   
