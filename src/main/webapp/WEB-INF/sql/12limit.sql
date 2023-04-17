0d61094ba5048697bcd5eb5b8927476e273c849d
SeungEun Baek <sebaek@gmail.com>
2023-04-17 09:48:36 +0900

    limit

--- src/main/webapp/WEB-INF/sql/12limit.sql
+++ src/main/webapp/WEB-INF/sql/12limit.sql
@@ -18,8 +18,18 @@ ORDER BY Price DESC
 LIMIT 3;
 
 -- 나이 많은 직원 3명 조회
+SELECT * FROM Employees ORDER BY BirthDate LIMIT 3;
 
 -- 브라질 고객중 아무나 3명 조회
+SELECT * FROM Customers
+WHERE Country = 'Brazil'
+LIMIT 3;
 
 -- 3번 카테고리 상품 중 가격이 싼 상품 2개 조회
+SELECT * FROM Products
+WHERE CategoryID = 3
+ORDER BY Price ASC
+LIMIT 2;
+
+
 
