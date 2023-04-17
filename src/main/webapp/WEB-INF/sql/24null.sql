240178b507ab2e29ecab8f4802e49ec1a25f35e2
SeungEun Baek <sebaek@gmail.com>
2023-04-17 15:32:21 +0900

    null 연산

---
+++ src/main/webapp/WEB-INF/sql/24null.sql
@@ -0,0 +1,8 @@
+-- null 과의 연산은 모두 false
+SELECT * FROM Products WHERE CategoryID = 2; -- 14
+SELECT * FROM Products 
+WHERE CategoryID = 2 
+   AND Price >= 25.00; -- 4
+SELECT * FROM Products 
+WHERE CategoryID = 2 
+   AND Price < 25.00; -- 8
