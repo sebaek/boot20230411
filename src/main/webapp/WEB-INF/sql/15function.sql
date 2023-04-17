779ee00c2a2096fc702fd9996764c6acb8f32241
SeungEun Baek <sebaek@gmail.com>
2023-04-17 10:30:21 +0900

    min, max

---
+++ src/main/webapp/WEB-INF/sql/15function.sql
@@ -0,0 +1,12 @@
+-- MIN, MAX
+
+SELECT MIN(CustomerID) FROM Customers;
+SELECT MAX(CustomerID) FROM Customers;
+SELECT MIN(Price) FROM Products;
+SELECT MAX(Price) FROM Products;
+
+-- 가장 나이 많은 직원의 생년월일
+-- 가장 어린 직원의 생년월일
+
+-- 3번 카테고리에 가장 비싼 상품의 가격
+-- 5번 카테고리에 가장 싼 상품의 가격
