8470e876c18ea290a1e94177f97ee39ae8c64329
SeungEun Baek <sebaek@gmail.com>
2023-04-17 10:38:09 +0900

    sum, avg

--- src/main/webapp/WEB-INF/sql/15function.sql
+++ src/main/webapp/WEB-INF/sql/15function.sql
@@ -6,7 +6,12 @@ SELECT MIN(Price) FROM Products;
 SELECT MAX(Price) FROM Products;
 
 -- 가장 나이 많은 직원의 생년월일
+SELECT MIN(BirthDate) FROM Employees;
 -- 가장 어린 직원의 생년월일
+SELECT MAX(BirthDate) FROM Employees;
 
 -- 3번 카테고리에 가장 비싼 상품의 가격
+SELECT MAX(Price) FROM Products WHERE CategoryId = 3;
 -- 5번 카테고리에 가장 싼 상품의 가격
+SELECT MIN(Price) FROM Products WHERE CategoryID = 5;
+
