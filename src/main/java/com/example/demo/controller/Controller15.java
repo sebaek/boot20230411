6aaf6ff9bbebf01feb29f404c7663e9b0f1eab0d
SeungEun Baek <sebaek@gmail.com>
2023-04-18 09:29:09 +0900

    insert

---
+++ src/main/java/com/example/demo/controller/Controller15.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import java.sql.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("sub15")
+public class Controller15 {
+
+	@Value("${spring.datasource.url}")
+	private String url;
+	@Value("${spring.datasource.username}")
+	private String name;
+	@Value("${spring.datasource.password}")
+	private String password;
+
+	@RequestMapping("link1")
+	public void method1() throws Exception {
+		// 새 고객데이터 추가
+		String sql = """
+				INSERT INTO Customers (CustomerID, CustomerName, City)
+				VALUES (93, '서태웅', '부산')
+				""";
+
+		// INSERT, UPDATE, DELETE는
+		// Statement의 executeUpdate 메소드 사용
+		// 리턴값은 영향을 미친 레코드 수
+		
+		Connection con = DriverManager.getConnection(url, name, password);
+		Statement stmt = con.createStatement();
+		int count = stmt.executeUpdate(sql);
+		
+		try (con; stmt; ) {
+			
+		}
+		
+		System.out.println(count + "개 행 추가됨");
+		
+	}
+	
+	// 경로: /sub15/link2 
+	// 10번째 직원 추가
+	// 컬럼 : EmployeeID, LastName, FirstName 
+	
+	
+}
+
+
+
+
