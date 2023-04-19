d3876d8b312294f4c64799e76cfa2081b755863e
SeungEun Baek <sebaek@gmail.com>
2023-04-19 14:02:16 +0900

    wnfakwcna

--- src/main/java/com/example/demo/controller/Controller18.java
+++ src/main/java/com/example/demo/controller/Controller18.java
@@ -18,7 +18,7 @@ public class Controller18 {
 	private String password;
 
 	// 트랜잭션 없이 모두 실행
-	@RequestMapping("link1") 
+	@RequestMapping("link1")
 	public void method1() {
 		String sql1 = """
 				UPDATE Bank
@@ -35,7 +35,7 @@ public class Controller18 {
 				Connection con = DriverManager.getConnection(url, username, password);
 				Statement stmt1 = con.createStatement();
 				Statement stmt2 = con.createStatement();) {
-			
+
 			stmt1.executeUpdate(sql1);
 			stmt2.executeUpdate(sql2);
 
@@ -45,9 +45,9 @@ public class Controller18 {
 		}
 
 	}
-	
+
 	// 트랜잭션 설정없이 중간에 exception 발생
-	@RequestMapping("link2") 
+	@RequestMapping("link2")
 	public void method2() {
 		String sql1 = """
 				UPDATE Bank
@@ -64,12 +64,12 @@ public class Controller18 {
 				Connection con = DriverManager.getConnection(url, username, password);
 				Statement stmt1 = con.createStatement();
 				Statement stmt2 = con.createStatement();) {
-			
+
 			stmt1.executeUpdate(sql1);
-			
+
 			// exception 발생
-			int a = 3 / 0; 
-			
+			int a = 3 / 0;
+
 			stmt2.executeUpdate(sql2);
 
 			System.out.println("이체 완료");
@@ -78,9 +78,9 @@ public class Controller18 {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// 트랜잭션 설정 후 모두 성공
-	@RequestMapping("link3") 
+	@RequestMapping("link3")
 	public void method3() {
 		String sql1 = """
 				UPDATE Bank
@@ -98,10 +98,10 @@ public class Controller18 {
 				Statement stmt1 = con.createStatement();
 				Statement stmt2 = con.createStatement();) {
 			con.setAutoCommit(false);
-			
+
 			stmt1.executeUpdate(sql1);
 			stmt2.executeUpdate(sql2);
-			
+
 			con.commit();
 
 			System.out.println("이체 완료");
@@ -110,48 +110,41 @@ public class Controller18 {
 			e.printStackTrace();
 		}
 	}
-	
-		// 트랜잭션 설정 후 모두 실패 (중간에 exception 발생시)
-		@RequestMapping("link4") 
-		public void method4() throws Exception {
-			String sql1 = """
-					UPDATE Bank
-					SET money = money - 5000
-					WHERE customerName = 'A'
-					""";
-			String sql2 = """
-					UPDATE Bank
-					SET money = money + 5000
-					WHERE customerName = 'B'
-					""";
 
-			Connection con = DriverManager.getConnection(url, username, password);
-			try ( 	con;
-					Statement stmt1 = con.createStatement();
-					Statement stmt2 = con.createStatement();) {
-				con.setAutoCommit(false);
-				
-				stmt1.executeUpdate(sql1);
-				
-				// exception 발생
-				int a = 5 / 0;
-				
-				stmt2.executeUpdate(sql2);
-				
-				con.commit();
+	// 트랜잭션 설정 후 모두 실패 (중간에 exception 발생시)
+	@RequestMapping("link4")
+	public void method4() throws Exception {
+		String sql1 = """
+				UPDATE Bank
+				SET money = money - 5000
+				WHERE customerName = 'A'
+				""";
+		String sql2 = """
+				UPDATE Bank
+				SET money = money + 5000
+				WHERE customerName = 'B'
+				""";
 
-				System.out.println("이체 완료");
-			} catch (Exception e) {
-				System.out.println("이체 실패");
-				con.rollback();
-				e.printStackTrace();
-			}
+		Connection con = DriverManager.getConnection(url, username, password);
+		try (con;
+				Statement stmt1 = con.createStatement();
+				Statement stmt2 = con.createStatement();) {
+			con.setAutoCommit(false);
+
+			stmt1.executeUpdate(sql1);
+
+			// exception 발생
+			int a = 5 / 0;
+
+			stmt2.executeUpdate(sql2);
+
+			con.commit();
+
+			System.out.println("이체 완료");
+		} catch (Exception e) {
+			System.out.println("이체 실패");
+			con.rollback();
+			e.printStackTrace();
 		}
+	}
 }
-
-
-
-
-
-
-
