b94461570ae9d6e187429fd7163e300eb76f7e56
SeungEun Baek <sebaek@gmail.com>
2023-04-19 12:25:25 +0900

    데이터베이스 만들기

---
+++ src/main/webapp/WEB-INF/sql/31database.sql
@@ -0,0 +1,25 @@
+-- DATABASE(Schema) : 테이블이 있는 곳
+
+-- CREATE DATABASE : database 만들기
+CREATE DATABASE mydb;
+USE mydb;
+CREATE TABLE MyTable11 (
+	Title VARCHAR(10),
+    Price INT
+);
+
+-- DROP DATABASE 데이터베이스 지우기 
+-- 주의*10000000000000000000000000 
+DROP DATABASE mydb;
+
+USE w3schools;
+
+-- mydb1 데이터베이스 만들기
+-- mydb1 에 새로운 테이블 만들기
+-- mydb1 데이터베이스 삭제하기
+-- w3schools 데이터베이스 사용하기
+
+
+
+
+
