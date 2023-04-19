075b78d70c6830bc787b907f0c8c74be715b1276
SeungEun Baek <sebaek@gmail.com>
2023-04-19 12:45:34 +0900

    테이블 만들기

--- src/main/webapp/WEB-INF/sql/31database.sql
+++ src/main/webapp/WEB-INF/sql/31database.sql
@@ -15,10 +15,23 @@ DROP DATABASE mydb;
 USE w3schools;
 
 -- mydb1 데이터베이스 만들기
+CREATE DATABASE mydb1;
 -- mydb1 에 새로운 테이블 만들기
+USE mydb1;
+CREATE TABLE MYTABLE00 (
+	COL INT,
+    COL2 VARCHAR(255)
+);
 -- mydb1 데이터베이스 삭제하기
+DROP DATABASE mydb1; -- 주의*99999999999
 -- w3schools 데이터베이스 사용하기
+USE w3schools;
 
+SELECT * FROM MyTable10;
+
+USE test;
+SELECT * FROM MyTable10; -- 현재 DATABASE
+SELECT * FROM w3schools.MyTable10; -- 다른 DB
 
 
 
---
+++ src/main/webapp/WEB-INF/sql/32drop-table.sql
@@ -0,0 +1,26 @@
+USE w3schools;
+
+-- DROP TABLE 테이블 삭제 
+-- DANGER*999999999999999999
+
+DESC MyTable01;
+
+DROP TABLE MyTable01; -- 주의!!!!!!!!
+
+-- 연습 : MyTable02, MyTable03 테이블 drop
+DROP TABLE MyTable02; -- 주의!!!!
+DROP TABLE MyTable03; -- 주의!!!!
+
+-- TRUNCATE : 테이블 유지, 데이터 삭제
+SELECT * FROM MyTable04;
+DESC MyTable04;
+TRUNCATE MyTable04; -- 주의!!!!!!!!!!!!
+
+
+
+
+
+
+
+
+
