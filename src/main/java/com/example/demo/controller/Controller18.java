f5bc1c77a90365537656021d8e91d75134768893
SeungEun Baek <sebaek@gmail.com>
2023-04-19 10:33:32 +0900

    트랜잭션

--- src/main/java/com/example/demo/controller/Controller18.java
+++ src/main/java/com/example/demo/controller/Controller18.java
@@ -77,6 +77,81 @@ public class Controller18 {
 			System.out.println("이체 실패");
 			e.printStackTrace();
 		}
-
 	}
+	
+	// 트랜잭션 설정 후 모두 성공
+	@RequestMapping("link3") 
+	public void method3() {
+		String sql1 = """
+				UPDATE Bank
+				SET money = money - 5000
+				WHERE customerName = 'A'
+				""";
+		String sql2 = """
+				UPDATE Bank
+				SET money = money + 5000
+				WHERE customerName = 'B'
+				""";
+
+		try (
+				Connection con = DriverManager.getConnection(url, username, password);
+				Statement stmt1 = con.createStatement();
+				Statement stmt2 = con.createStatement();) {
+			con.setAutoCommit(false);
+			
+			stmt1.executeUpdate(sql1);
+			stmt2.executeUpdate(sql2);
+			
+			con.commit();
+
+			System.out.println("이체 완료");
+		} catch (Exception e) {
+			System.out.println("이체 실패");
+			e.printStackTrace();
+		}
+	}
+	
+		// 트랜잭션 설정 후 모두 실패 (중간에 exception 발생시)
+		@RequestMapping("link4") 
+		public void method4() throws Exception {
+			String sql1 = """
+					UPDATE Bank
+					SET money = money - 5000
+					WHERE customerName = 'A'
+					""";
+			String sql2 = """
+					UPDATE Bank
+					SET money = money + 5000
+					WHERE customerName = 'B'
+					""";
+
+			Connection con = DriverManager.getConnection(url, username, password);
+			try ( 	con;
+					Statement stmt1 = con.createStatement();
+					Statement stmt2 = con.createStatement();) {
+				con.setAutoCommit(false);
+				
+				stmt1.executeUpdate(sql1);
+				
+				// exception 발생
+				int a = 5 / 0;
+				
+				stmt2.executeUpdate(sql2);
+				
+				con.commit();
+
+				System.out.println("이체 완료");
+			} catch (Exception e) {
+				System.out.println("이체 실패");
+				con.rollback();
+				e.printStackTrace();
+			}
+		}
 }
+
+
+
+
+
+
+
