634e19982d47d33342161b1cf64ecdaa90df945f
SeungEun Baek <sebaek@gmail.com>
2023-04-19 14:21:06 +0900

    add column

---
+++ src/main/webapp/WEB-INF/sql/34alter-table.sql
@@ -0,0 +1,29 @@
+USE w3schools;
+-- ALTER TABLE : 테이블 변경
+CREATE TABLE MyTable13 (
+	col1 INT,
+    col2 INT
+);
+
+-- 컬럼 추가 ADD COLUMN
+ALTER TABLE MyTable13 
+ADD COLUMN col3 VARCHAR(10);
+DESC MyTable13;
+ALTER TABLE MyTable13
+ADD COLUMN col4 DEC(10, 2);
+-- 연습) MyTable13에 5번째 컬럼 추가
+ALTER TABLE MyTable13 ADD COLUMN col5 INT;
+DESC MyTable13;
+
+-- 첫번째 컬럼으로 추가
+ALTER TABLE MyTable13 ADD COLUMN col6 INT FIRST;
+ALTER TABLE MyTable13 ADD COLUMN col7 INT FIRST;
+DESC MyTable13;
+
+
+
+
+
+
+
+
