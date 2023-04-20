5ab0d64e68ba10f37357d9da2bd8cb17cefa042c
SeungEun Baek <sebaek@gmail.com>
2023-04-20 09:36:04 +0900

    data type

--- src/main/java/com/example/demo/controller/Controller19.java
+++ src/main/java/com/example/demo/controller/Controller19.java
@@ -15,7 +15,7 @@ public class Controller19 {
 	private String username;
 	@Value("${spring.datasource.password}")
 	private String password;
-	
+
 	@RequestMapping("link1")
 	public void method1() throws Exception {
 		String sql = """
@@ -24,16 +24,15 @@ public class Controller19 {
 				""";
 		try (
 				Connection con = DriverManager.getConnection(url, username, password);
-				PreparedStatement pstmt = con.prepareStatement(sql);
-				) {
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
 			pstmt.setInt(1, 99);
 			pstmt.setString(2, "hello");
 			int cnt = pstmt.executeUpdate();
 			System.out.println(cnt + "개 행 입력됨");
 		}
-		
+
 	}
-	
+
 	@RequestMapping("link2")
 	public void method2() throws Exception {
 		String sql = """
@@ -42,27 +41,53 @@ public class Controller19 {
 				""";
 		try (
 				Connection con = DriverManager.getConnection(url, username, password);
-				PreparedStatement pstmt = con.prepareStatement(sql);
-				) {
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
 //			pstmt.setInt(1, 99);
 //			pstmt.setString(2, "hello");
 			pstmt.setString(1, "888");
 			pstmt.setInt(2, 12345);
-			
+
 			int cnt = pstmt.executeUpdate();
 			System.out.println(cnt + "개 행 입력됨");
 		}
-		
+
+	}
+
+	@RequestMapping("link3")
+	public void method3() throws Exception {
+		String sql = """
+				INSERT INTO MyTable31 (Col1, Col2)
+				VALUES (?, ?)
+				""";
+		try (
+				Connection con = DriverManager.getConnection(url, username, password);
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
+			pstmt.setString(1, "1923-03-01");
+			pstmt.setString(2, "1811-11-12 23:50:49");
+
+			int cnt = pstmt.executeUpdate();
+			System.out.println(cnt + "개 행 입력됨");
+		}
+
+	}
+
+	@RequestMapping("link4")
+	public void method4() throws Exception {
+		String sql = """
+				INSERT INTO MyTable31 (Col1, Col2)
+				VALUES (?, ?)
+				""";
+		try (
+				Connection con = DriverManager.getConnection(url, username, password);
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
+//			pstmt.setString(1, "1923-03-01");
+//			pstmt.setString(2, "1811-11-12 23:50:49");
+			pstmt.setDate(1, Date.valueOf("2023-03-01"));
+			pstmt.setTimestamp(2, Timestamp.valueOf("2000-01-01 22:22:11"));
+
+			int cnt = pstmt.executeUpdate();
+			System.out.println(cnt + "개 행 입력됨");
+		}
+
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
