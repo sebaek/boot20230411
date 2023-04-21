26e045592945211483ae4ea7065d284e05fee99b
SeungEun Baek <sebaek@gmail.com>
2023-04-21 10:45:03 +0900

    insert

--- src/main/java/com/example/demo/mapper/Mapper03.java
+++ src/main/java/com/example/demo/mapper/Mapper03.java
@@ -10,7 +10,21 @@ public interface Mapper03 {
 			VALUES (#{val1}, #{val2})
 			""")
 	int sql1(Integer val1, String val2);
+	
+	@Insert("""
+			INSERT INTO MyTable35 (Col1, Col2)
+			VALUES (#{v1}, #{v2})
+			""")
+	int sql2(Double v1, String v2);
+	// MyTable35 만들기
+	// controller22.method2에서 mapper03.sql2 메소드 호출
 }
+
+
+
+
+
+
 
 
 
