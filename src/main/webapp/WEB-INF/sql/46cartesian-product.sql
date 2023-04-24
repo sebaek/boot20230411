0f0edd21cadb9400f9bae938cb99ee19b1006189
SeungEun Baek <sebaek@gmail.com>
2023-04-24 14:20:34 +0900

    join

--- src/main/webapp/WEB-INF/sql/46cartesian-product.sql
+++ src/main/webapp/WEB-INF/sql/46cartesian-product.sql
@@ -7,5 +7,19 @@ FROM Products JOIN Categories; -- 616
 
 SELECT * FROM Products JOIN Categories;
 
+-- ON : 조인 조건
+SELECT * FROM Products JOIN Categories 
+ON Products.CategoryId = Categories.CategoryId;
+
+-- 조인 후 컬럼 수 : 왼쪽테이블컬럼수 + 오른쪽테이블컬럼수
+DESC Products; -- 6개 컬럼
+DESC Categories; -- 3개 컬럼
+SELECT * FROM Products JOIN Categories
+ON Products.CategoryID = Categories.CategoryId; -- 9개 컬럼
+
+SELECT ProductName, CategoryName 
+FROM Products JOIN Categories
+ON Products.CategoryId = Categories.CategoryId;
+
 
 
