57d2ed16ebaac84a7e9c22fafb645c5f6048d1f4
SeungEun Baek <sebaek@gmail.com>
2023-04-24 09:54:20 +0900

    정규화

---
+++ src/main/webapp/WEB-INF/sql/43normalization.sql
@@ -0,0 +1,13 @@
+-- normalization (정규화)
+-- 1NF (first normal form) 첫번째 정규화
+-- 규칙1) 열은 원자적 값만을 포함한다.
+-- 규칙2) 같은 데이터가 여러 열에 반복되지 말아야 한다.
+
+
+
+
+
+-- 2NF (second normal form) 두번째 정규화
+-- 3NF (third normal form)세번째 정규화
+
+
