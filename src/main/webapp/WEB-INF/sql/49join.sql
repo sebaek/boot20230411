ebd1e1e063ed45dff92516e4f585b77bb01bdee1
SeungEun Baek <sebaek@gmail.com>
2023-04-24 15:26:15 +0900

    join

--- src/main/webapp/WEB-INF/sql/49join.sql
+++ src/main/webapp/WEB-INF/sql/49join.sql
@@ -10,5 +10,14 @@ Products JOIN Categories JOIN Suppliers; -- 17개 열
 SELECT COUNT(*) FROM 
 Products JOIN Categories JOIN Suppliers; -- 17864개 행
 
-
+SELECT * FROM 
+Products p JOIN Categories c ON p.CategoryID = c.CategoryID
+           JOIN Suppliers s ON p.SupplierID = s.SupplierID; -- 77개행
+           
+-- 예) 1번 상품의 상품명, 카테고리명, 공급자명
+SELECT p.ProductName, c.CategoryName, s.SupplierName
+FROM Products p JOIN Categories c ON p.CategoryID = c.CategoryID
+                JOIN Suppliers s ON p.SupplierID = s.SupplierID
+WHERE p.ProductID = 1;
+           
 
