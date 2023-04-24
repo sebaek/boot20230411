74d328c94bc5d9934330e4f19f5aefbeeae6f410
SeungEun Baek <sebaek@gmail.com>
2023-04-24 12:26:21 +0900

    외래키

---
+++ src/main/webapp/WEB-INF/sql/45join.sql
@@ -0,0 +1,48 @@
+DROP DATABASE w3schools;
+USE w3schools;
+SELECT * FROM Products;
+DESC Products;
+SELECT * FROM Categories;
+ALTER TABLE Products
+ADD FOREIGN KEY (CategoryId) 
+REFERENCES Categories(CategoryId);
+DESC Products;
+ALTER TABLE Products
+ADD FOREIGN KEY (SupplierId)
+REFERENCES Suppliers(SupplierId);
+DESC Categories;
+DESC Customers;
+DESC Employees;
+DESC OrderDetails;
+ALTER TABLE OrderDetails
+ADD FOREIGN KEY (OrderId) 
+REFERENCES Orders(OrderId);
+ALTER TABLE OrderDetails
+ADD FOREIGN KEY (ProductId) 
+REFERENCES Products(ProductId);
+DESC Orders;
+ALTER TABLE Orders
+ADD FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerId);
+ALTER TABLE Orders
+ADD FOREIGN KEY (EmployeeID)
+REFERENCES Employees(EmployeeID);
+ALTER TABLE Orders
+ADD FOREIGN KEY (ShipperId)
+REFERENCES Shippers(ShipperId);
+DESC Shippers;
+DESC Suppliers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
