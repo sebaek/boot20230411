11db4324a71591d22453438b4fbab0a4063aeb9d
SeungEun Baek <sebaek@gmail.com>
2023-04-24 15:15:46 +0900

    join

--- src/main/webapp/WEB-INF/sql/48join.sql
+++ src/main/webapp/WEB-INF/sql/48join.sql
@@ -7,3 +7,12 @@ SELECT * FROM Orders o RIGHT JOIN Customers c
 ON o.CustomerID = c.CustomerID
 WHERE o.OrderId IS NULL; -- 주문한적 없는 고객
 
+-- 예) 주문을 처리한 적 없는 직원 조회
+DESC Orders;
+SELECT e.LastName, e.FirstName FROM Orders o RIGHT JOIN Employees e 
+ON o.EmployeeID = e.EmployeeID
+WHERE o.OrderId IS NULL;
+
+
+
+
