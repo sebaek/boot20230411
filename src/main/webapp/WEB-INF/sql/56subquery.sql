6857c31284899eacb6b8466fdb0f8a1577f68c06
SeungEun Baek <sebaek@gmail.com>
2023-04-25 12:33:56 +0900

    서브쿼리

--- src/main/webapp/WEB-INF/sql/56subquery.sql
+++ src/main/webapp/WEB-INF/sql/56subquery.sql
@@ -6,6 +6,14 @@ SELECT lastName, firstName
 FROM (SELECT lastName, firstName FROM Employees) emp
 WHERE lastName >= 'd' AND lastName < 'e';
 
-
+-- 고객별 소비금액
+SELECT AVG(구매금액) FROM 
+(SELECT c.CustomerID, c.CustomerName, SUM(p.Price * od.Quantity) 구매금액 
+FROM
+	Orders o JOIN OrderDetails od ON o.OrderID = od.OrderID
+		     JOIN Customers c ON o.CustomerID = c.CustomerID
+             JOIN Products p ON od.ProductID = p.ProductID
+GROUP BY c.CustomerID
+ORDER BY 구매금액 DESC) t1;
 
 
