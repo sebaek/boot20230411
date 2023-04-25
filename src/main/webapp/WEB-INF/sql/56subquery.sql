96691fe8d4c86364804dce7ade0856ce7e0735e1
SeungEun Baek <sebaek@gmail.com>
2023-04-25 12:43:51 +0900

    서브쿼리

--- src/main/webapp/WEB-INF/sql/56subquery.sql
+++ src/main/webapp/WEB-INF/sql/56subquery.sql
@@ -18,6 +18,22 @@ ORDER BY 구매금액 DESC) t1;
 
 -- (직원별 매출액)의 평균
 
+-- 카테고리별 평균상품가격 조회 (평균상품가격이 30.00 달러 이상인 것만)
+SELECT CategoryID, AVG(Price)
+FROM Products
+GROUP BY CategoryID
+HAVING AVG(Price) >= 30.00;
+
+SELECT CategoryID, average
+FROM
+(SELECT CategoryID, AVG(Price) average
+FROM Products
+GROUP BY CategoryID) T1
+WHERE average >= 30.00;
+
+
+
+
 
 
 
