e5c7a3ff8b9af94d4f2079fd2f63010ebb7362c9
SeungEun Baek <sebaek@gmail.com>
2023-04-25 09:34:39 +0900

    leetcode 175

--- src/main/webapp/WEB-INF/sql/50leetcode175.sql
+++ src/main/webapp/WEB-INF/sql/50leetcode175.sql
@@ -23,6 +23,17 @@ INSERT INTO Address VALUES (2, 3, 'Leetcode', 'California');
 -- 문제) 각 인물의 firstName, lastName, city, state 를 조회
 -- 하는 쿼리 작성, 주소가 없으면 city, state에 null로
 
+SELECT p.firstName, p.lastName, a.city, a.state FROM 
+Person p LEFT JOIN Address a ON p.personId = a.personId;
+
+
+
+
+
+
+
+
+
 
 
 
