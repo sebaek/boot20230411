aeb247bdea65f19f0bdc8bc5e8d882c7ab3d2ddf
SeungEun Baek <sebaek@gmail.com>
2023-04-25 09:24:08 +0900

    Update 50leetcode175.sql

--- src/main/webapp/WEB-INF/sql/50leetcode175.sql
+++ src/main/webapp/WEB-INF/sql/50leetcode175.sql
@@ -20,7 +20,8 @@ CREATE TABLE Address (
 INSERT INTO Address VALUES (1, 2, 'New York City', 'New York');
 INSERT INTO Address VALUES (2, 3, 'Leetcode', 'California');
 
-
+-- 문제) 각 인물의 firstName, lastName, city, state 를 조회
+-- 하는 쿼리 작성, 주소가 없으면 city, state에 null로
 
 
 
