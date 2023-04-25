e5aeaf8dc6bdac0e1fdd038d9bb7196e2ef63d7f
SeungEun Baek <sebaek@gmail.com>
2023-04-25 10:16:37 +0900

    leetcode 181

---
+++ src/main/webapp/WEB-INF/sql/52leetcode181.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE leetcode181;
+USE leetcode181;
+CREATE TABLE Employee(
+	id INT PRIMARY KEY,
+    name VARCHAR(200),
+    salary INT,
+    managerId INT
+);
+INSERT INTO Employee VALUES (1, 'Joe', 70000, 3);
+INSERT INTO Employee VALUES (2, 'Henry', 80000, 4);
+INSERT INTO Employee VALUES (3, 'Sam', 60000, NULL);
+INSERT INTO Employee VALUEs (4, 'Max', 90000, NULL);
+
+SELECT * FROM Employee;
+
+
+
