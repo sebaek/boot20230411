81d82c6f8dd03f96b588894fb9d4dee05b2b8217
SeungEun Baek <sebaek@gmail.com>
2023-04-27 14:41:52 +0900

    sql

--- src/main/webapp/WEB-INF/sql/58pagination.sql
+++ src/main/webapp/WEB-INF/sql/58pagination.sql
@@ -11,10 +11,16 @@ DESC Customers;
 -- 1 페이지
 -- 페이지당 20개 레코드
 SELECT * FROM Customers
+ORDER BY CustomerID DESC;
+SELECT * FROM Customers
 ORDER BY CustomerID DESC
 LIMIT 0, 20; -- 1 페이지
 
 SELECT * FROM Customers
 ORDER BY CustomerID DESC
 LIMIT 20, 20; -- 2 페이지
+
+SELECT * FROM Customers
+ORDER BY CustomerID DESC
+LIMIT 40, 20; -- 3 페이지
 
