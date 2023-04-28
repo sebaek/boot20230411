a91c6915b8ffe1c11fbea74754a0941b2b8c01e2
SeungEun Baek <sebaek@gmail.com>
2023-04-28 14:30:48 +0900

    bind

--- src/main/java/com/example/demo/controller/Controller27.java
+++ src/main/java/com/example/demo/controller/Controller27.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.domain.*;
 import com.example.demo.mapper.*;
 
 @Controller
@@ -13,33 +16,25 @@ public class Controller27 {
 
 	@Autowired
 	private Mapper07 mapper;
-	
-	
+
 	// 경로 : /sub27/link1?s=ell
 	@GetMapping("link1")
 	public String method1(Model model,
 			@RequestParam(value = "s", defaultValue = "") String keyword) {
-		
+
 		model.addAttribute("customerList", mapper.sql1(keyword));
-		
+
 		return "/sub13/link1";
 	}
-	
+
 	// 경로 : /sub27/link2?search=eve
 	@GetMapping("link2")
-	public String method2() {
+	public String method2(Model model, @RequestParam(value = "search", defaultValue = "") String s) {
 		// search 리퀘스트 파라미터로 직원 lastName, firstName검색
-		
-		mapper.sql2();
-		
+
+		List<Employee> list = mapper.sql2(s);
+		model.addAttribute("employeeList", list);
+
 		return "/sub13/link2"; // 직원 lastName, firstName
 	}
 }
-
-
-
-
-
-
-
-
--- src/main/java/com/example/demo/mapper/Mapper07.java
+++ src/main/java/com/example/demo/mapper/Mapper07.java
@@ -26,6 +26,24 @@ public interface Mapper07 {
 			</script>
 			""")
 	List<Customer> sql1(String keyword);
+
+	@Select("""
+			<script>
+			<bind name="p" value="'%' + s + '%'" />
+			
+			SELECT
+				EmployeeId id,
+				FirstName,
+				LastName
+			FROM
+				Employees
+			WHERE
+				FirstName LIKE #{p}
+				OR LastName LIKE #{p}
+			ORDER BY id DESC
+			</script>
+			""")
+	List<Employee> sql2(String s);
 }
 
 
