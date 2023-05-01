60260a0a8e5976a96e1f51be6a9c25c7cdd637da
SeungEun Baek <sebaek@gmail.com>
2023-05-01 14:15:57 +0900

    foreach

--- src/main/java/com/example/demo/controller/Controller28.java
+++ src/main/java/com/example/demo/controller/Controller28.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.domain.*;
 import com.example.demo.mapper.*;
 
 @Controller
@@ -19,7 +20,19 @@ public class Controller28 {
 	public void method1() {
 		mapper.sql1(List.of("UK", "USA"));
 	}
+	
+	@GetMapping("link2")
+	public void method2() {
+		List<Supplier> list1 = mapper.sql2(List.of("UK", "USA", "Japan"));
+		list1.forEach(System.out::print);
+		
+		List<Supplier> list2 = mapper.sql2(List.of("Brazil", "Germany"));
+		list2.forEach(System.out::print);
+	}
 }
+
+
+
 
 
 
