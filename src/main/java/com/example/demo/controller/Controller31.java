42bc48bccec89bc5c1bb9116636a232192cda505
SeungEun Baek <sebaek@gmail.com>
2023-05-02 14:57:03 +0900

    Update Controller31.java

--- src/main/java/com/example/demo/controller/Controller31.java
+++ src/main/java/com/example/demo/controller/Controller31.java
@@ -17,7 +17,6 @@ public class Controller31 {
 	// 경로: localhost:8082/sub31/link1?id=3
 	@GetMapping("link1")
 	public void method1(int id) {
-		// 1번 카테고리 정보
 		Category category = mapper.sql1(id);
 		
 		System.out.println(category);
@@ -27,7 +26,6 @@ public class Controller31 {
 	// 경로: localhost:8082/sub31/link2?id=3
 		@GetMapping("link2")
 		public void method2(int id) {
-			// 1번 카테고리 정보
 			Category category = mapper.sql2(id);
 			
 			System.out.println(category);
