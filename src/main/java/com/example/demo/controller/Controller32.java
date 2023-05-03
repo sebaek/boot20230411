bf914849a0c0f8acf4a6a0983582837fbcb62064
SeungEun Baek <sebaek@gmail.com>
2023-05-03 15:39:57 +0900

    delete object

--- src/main/java/com/example/demo/controller/Controller32.java
+++ src/main/java/com/example/demo/controller/Controller32.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.annotation.*;
 import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.*;
@@ -13,48 +14,59 @@ import software.amazon.awssdk.services.s3.model.*;
 @Controller
 @RequestMapping("sub32")
 public class Controller32 {
-	
+
 	@Value("${aws.accessKeyId}")
 	private String accessKey;
-	
+
 	@Value("${aws.secretAccessKey}")
 	private String secretKey;
 
 	@Value("${aws.s3.bucketName}")
 	private String bucketName;
-	
+
+	private S3Client s3;
+
+	@PostConstruct
+	public void init() {
+		AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+		AwsCredentialsProvider provider = StaticCredentialsProvider.create(credentials);
+		Region region = Region.AP_NORTHEAST_2;
+
+		this.s3 = S3Client.builder()
+				.credentialsProvider(provider)
+				.region(region)
+				.build();
+
+	}
+
 	@GetMapping("link1")
 	public void method1() {
 		System.out.println(accessKey);
 		System.out.println(secretKey);
-		
-		Region region = Region.AP_NORTHEAST_2;
+
 
 		String key = "test/myFile.txt";
-		
+
 		String content = "새로운 파일 내용물";
-		
-		AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-		AwsCredentialsProvider provider = StaticCredentialsProvider.create(credentials);
-		
-		S3Client s3 = S3Client.builder()
-				.credentialsProvider(provider)
-                .region(region)
-                .build();
-		
+
 		// s3에 파일 업로드
 		PutObjectRequest objectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .acl(ObjectCannedACL.PUBLIC_READ)
-                .key(key)
-                .build();
+				.bucket(bucketName)
+				.acl(ObjectCannedACL.PUBLIC_READ)
+				.key(key)
+				.build();
 
-        s3.putObject(objectRequest, RequestBody.fromBytes(content.getBytes()));
+		s3.putObject(objectRequest, RequestBody.fromBytes(content.getBytes()));
+	}
+
+	@GetMapping("link2")
+	public void method2() {
+		String key = "test/myFile.txt";
+		DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+				.bucket(bucketName)
+				.key(key)
+				.build();
+
+		s3.deleteObject(deleteObjectRequest);
 	}
 }
-
-
-
-
-
-
