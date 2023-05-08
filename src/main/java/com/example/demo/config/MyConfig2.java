7015876b98a0c78e2c862a49b2902d4a7eca121e
SeungEun Baek <sebaek@gmail.com>
2023-05-08 15:51:48 +0900

    login

--- src/main/java/com/example/demo/config/MyConfig2.java
+++ src/main/java/com/example/demo/config/MyConfig2.java
@@ -15,45 +15,48 @@ import org.springframework.security.web.*;
 public class MyConfig2 {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) 
+	public SecurityFilterChain securityFilterChain(HttpSecurity http)
 			throws Exception {
 		http.formLogin()
-			.loginPage("/sub33/mylogin")
-			.defaultSuccessUrl("/list", true)
-			.usernameParameter("id") // username
-			.passwordParameter("pw"); // password
-		
+				.loginPage("/sub33/mylogin")
+				.defaultSuccessUrl("/list", true)
+				.usernameParameter("id") // username
+				.passwordParameter("pw"); // password
+
 		http.csrf().disable();
-		
+
 		http.authorizeHttpRequests().requestMatchers("/abc").authenticated();
 		http.authorizeHttpRequests().anyRequest().permitAll();
-		
+
 		return http.build();
 	}
-	
+
 	@Bean
 	public UserDetailsService userDetailsService() {
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		UserDetails user1 = users.username("user1")
-				.password("pw1")
+		PasswordEncoder encoder = passwordEncoder();
+
+		String pw1 = encoder.encode("pw1");
+		String pw2 = encoder.encode("pw2");
+
+		System.out.println("pw1:" + pw1);
+		System.out.println("pw2:" + pw2);
+
+		UserDetails user1 = User.builder()
+				.username("user1")
+				.password(pw1)
 				.authorities(List.of())
 				.build();
-		UserDetails user2 = users.username("user2")
-				.password("pw2")
+		UserDetails user2 = User.builder()
+				.username("user2")
+				.password(pw2)
 				.authorities(List.of())
 				.build();
-		
+
 		return new InMemoryUserDetailsManager(user1, user2);
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 }
-
-
-
-
-
-
