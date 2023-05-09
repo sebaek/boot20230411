f368c94a3d5627e32982e5c1dc0bdfa1a69005a7
SeungEun Baek <sebaek@gmail.com>
2023-05-09 12:38:21 +0900

    authentication

--- src/main/java/com/example/demo/controller/Controller33.java
+++ src/main/java/com/example/demo/controller/Controller33.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,15 @@ public class Controller33 {
 	@GetMapping("loginSuccess")
 	public void loginSucess() {
 		
+	}
+	
+	@GetMapping("viewAuth")
+	public void viewAuth(Authentication authentication) {
+		
+		System.out.println("로그인 정보 확인 하기");
+		
+		System.out.println(authentication);
+		System.out.println(authentication.getName());
 	}
 }
 
