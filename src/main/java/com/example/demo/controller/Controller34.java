8df7fd4cd790154746164fca22b52098259f3b62
SeungEun Baek <sebaek@gmail.com>
2023-05-10 16:23:33 +0900

    ajax

---
+++ src/main/java/com/example/demo/controller/Controller34.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("sub34")
+public class Controller34 {
+	
+	@GetMapping("ajax1")
+	public void ajax1() {
+		
+	}
+
+	@GetMapping("link1")
+	public void method1() {
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
