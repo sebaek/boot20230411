7004e77733b5a08506899d631189e483800bad91
SeungEun Baek <sebaek@gmail.com>
2023-05-12 14:17:06 +0900

    응답 후 실행 메소드

---
+++ src/main/java/com/example/demo/controller/Controller39.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("sub39")
+public class Controller39 {
+
+	@GetMapping("view")
+	public void method1() {
+		
+	}
+}
